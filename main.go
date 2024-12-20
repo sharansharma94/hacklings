bde58752e3b432d36e5e3d859cff1e5d64a2ccf9
sharansharma94 <sharansharma19@gmail.com>
2024-12-20 20:12:06 +0530

    add TwoSum function implementation and corresponding tests; enhance file watcher to run tests on changes

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 
 	"github.com/fsnotify/fsnotify"
@@ -21,13 +22,17 @@ func addWatchers(watcher *fsnotify.Watcher, dir string) error {
 	})
 }
 
-func main() {
-	watcher, err := fsnotify.NewWatcher()
+func runTests(dir string) {
+	fmt.Printf("Running test cases...\n")
+	cmd := exec.Command("go", "test", dir)
+	output, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("Error running tests: %v\n", err)
 	}
-	defer watcher.Close()
+	fmt.Printf("Test output:\n%s\n", string(output))
+}
 
+func startWatcher(watcher *fsnotify.Watcher, dir string) {
 	go func() {
 		for {
 			select {
@@ -38,6 +43,7 @@ func main() {
 				log.Println("event:", event)
 				if event.Has(fsnotify.Write) {
 					log.Println("modified file:", event.Name)
+					runTests(dir) // Run tests for the modified file
 				}
 			case err, ok := <-watcher.Errors:
 				if !ok {
@@ -47,12 +53,26 @@ func main() {
 			}
 		}
 	}()
+}
 
-	fmt.Println("Watching for file changes...")
-	err = addWatchers(watcher, "./exercises")
+func main() {
+	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer watcher.Close()
+	const DIR = "./exercises"
+	const TEST_DIR = DIR + "/..."
+
+	startWatcher(watcher, TEST_DIR)
+
+	fmt.Println("Watching for file changes...")
+	err = addWatchers(watcher, DIR)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	runTests(TEST_DIR) // Run all tests initially
 
 	<-make(chan struct{})
 
